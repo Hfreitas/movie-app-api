baf543de158055c6711c889bcea1c5d966da25d5
Hfreitas <hfreitas86@gmail.com>
2020-09-19 13:47:35 -0300

    correção de retorno com parametro incorreto e acrescimo de checagem de existencia de nome do usuario

--- services/userServices.js
+++ services/userServices.js
@@ -7,17 +7,19 @@ const encryptPassword = async (password) => bcrypt.hash(password, saltRounds);
 
 const checkUserData = async (param = '', paramValue) => {
   const data = await user.findUser(param, paramValue);
-  return user ? user[param] : null;
+  return data ? data[param] : null;
 };
 
-const create = async ({ name, email, password }) => {
+const create = async ( name, email, password ) => {
   const checkUserEmail = await checkUserData('email', email);
+  const checkUserName= await checkUserData('name', name);
 
   if (checkUserEmail) throw new Error('user email alreadys registered');
+  if (checkUserName) throw new Error('user name alreadys registered');
 
   const hashPassword = await encryptPassword(password);
   const addUser = await user.createUser(name, email, hashPassword);
-  
+
   return { ...addUser };
 };
 
