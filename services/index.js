d22e85e9c86e9118711f5377011e9892d52655a2
Hfreitas <hfreitas86@gmail.com>
2020-09-19 13:49:56 -0300

    criado service para registro de usuário

--- services/index.js
+++ services/index.js
@@ -1,6 +1,8 @@
-const { searchMovieById, searchMovieByTitle } = require('./externalAPI');
+const { searchMovieById, searchMovieByTitle } = require('./APIServices');
+const { create } = require('./userServices');
 
 module.exports = {
   mediaById: searchMovieById,
   mediaByTitle: searchMovieByTitle,
+  addUser: create,
 };
