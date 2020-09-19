fb2fd1c6b2800f98f41e9a4257b136299b462474
Hfreitas <hfreitas86@gmail.com>
2020-09-19 13:48:27 -0300

    criada rota de registro do usuário

--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -1,6 +1,8 @@
 const OMDBroutes = require('./OMDBroutes');
+const userRoutes = require('./userRoutes');
 
 module.exports = {
   searchByTitle: OMDBroutes.getMoviesByTitle,
   searchByID: OMDBroutes.getMovieByID,
+  registryUser: userRoutes.registryUser,
 };
